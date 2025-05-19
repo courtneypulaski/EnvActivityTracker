03f6c92360b658d22897a485aead39b6cde94fd4
courtneypulaski <courtney.a.pulaski@gmail.com>
2025-05-18 20:52:13 -0400

    Update SQL table, UI things

---
+++ EnvActivityTracker/TrackerSQL/Tables/Environments.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Environments]
+(
+	[EnvCode] VARCHAR(5) NOT NULL PRIMARY KEY, 
+    [EnvName] VARCHAR(25) NOT NULL, 
+    [EnvDetails] VARCHAR(256) NULL, 
+    [CreateDateTime] DATETIME NOT NULL DEFAULT GETDATE() 
+)
--- EnvActivityTracker/TrackerUI/CreateEnvForm.Designer.cs
+++ EnvActivityTracker/TrackerUI/CreateEnvForm.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace TrackerUI
 {
-    partial class NewEnvForm
+    partial class CreateEnvForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,98 +23,106 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             envCodeLabel = new Label();
-            envCodeValue = new TextBox();
-            envNameValue = new TextBox();
             envNameLabel = new Label();
             envDetailsLabel = new Label();
-            textBox1 = new TextBox();
+            envCodeValue = new TextBox();
+            envNameValue = new TextBox();
+            envDetailsValue = new TextBox();
             createEnvButton = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // envCodeLabel
             // 
             envCodeLabel.AutoSize = true;
-            envCodeLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            envCodeLabel.Location = new Point(19, 33);
-            envCodeLabel.Margin = new Padding(5, 0, 5, 0);
+            envCodeLabel.Location = new Point(36, 74);
             envCodeLabel.Name = "envCodeLabel";
             envCodeLabel.Size = new Size(168, 25);
             envCodeLabel.TabIndex = 0;
             envCodeLabel.Text = "Environment Code";
             // 
-            // envCodeValue
-            // 
-            envCodeValue.Location = new Point(195, 30);
-            envCodeValue.Name = "envCodeValue";
-            envCodeValue.Size = new Size(172, 33);
-            envCodeValue.TabIndex = 1;
-            // 
-            // envNameValue
-            // 
-            envNameValue.Location = new Point(195, 100);
-            envNameValue.Name = "envNameValue";
-            envNameValue.Size = new Size(172, 33);
-            envNameValue.TabIndex = 3;
-            // 
             // envNameLabel
             // 
             envNameLabel.AutoSize = true;
-            envNameLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            envNameLabel.Location = new Point(19, 103);
-            envNameLabel.Margin = new Padding(5, 0, 5, 0);
+            envNameLabel.Location = new Point(36, 129);
             envNameLabel.Name = "envNameLabel";
             envNameLabel.Size = new Size(174, 25);
-            envNameLabel.TabIndex = 2;
+            envNameLabel.TabIndex = 1;
             envNameLabel.Text = "Environment Name";
             // 
             // envDetailsLabel
             // 
             envDetailsLabel.AutoSize = true;
-            envDetailsLabel.Location = new Point(19, 165);
+            envDetailsLabel.Location = new Point(36, 184);
             envDetailsLabel.Name = "envDetailsLabel";
             envDetailsLabel.Size = new Size(181, 25);
-            envDetailsLabel.TabIndex = 4;
+            envDetailsLabel.TabIndex = 2;
             envDetailsLabel.Text = "Environment Details";
             // 
-            // textBox1
+            // envCodeValue
             // 
-            textBox1.Location = new Point(19, 193);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(348, 127);
-            textBox1.TabIndex = 5;
+            envCodeValue.Location = new Point(234, 71);
+            envCodeValue.Name = "envCodeValue";
+            envCodeValue.Size = new Size(223, 33);
+            envCodeValue.TabIndex = 3;
+            // 
+            // envNameValue
+            // 
+            envNameValue.Location = new Point(234, 126);
+            envNameValue.Name = "envNameValue";
+            envNameValue.Size = new Size(223, 33);
+            envNameValue.TabIndex = 4;
+            // 
+            // envDetailsValue
+            // 
+            envDetailsValue.Location = new Point(36, 231);
+            envDetailsValue.Multiline = true;
+            envDetailsValue.Name = "envDetailsValue";
+            envDetailsValue.Size = new Size(421, 149);
+            envDetailsValue.TabIndex = 5;
             // 
             // createEnvButton
             // 
-            createEnvButton.Location = new Point(78, 348);
+            createEnvButton.Location = new Point(147, 416);
             createEnvButton.Name = "createEnvButton";
-            createEnvButton.Size = new Size(230, 61);
+            createEnvButton.Size = new Size(221, 50);
             createEnvButton.TabIndex = 6;
             createEnvButton.Text = "Create Environment";
             createEnvButton.UseVisualStyleBackColor = true;
             // 
-            // NewEnvForm
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(61, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 37);
+            label1.TabIndex = 7;
+            label1.Text = "New Environment";
+            // 
+            // CreateEnvForm
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(398, 445);
+            ClientSize = new Size(494, 508);
+            Controls.Add(label1);
             Controls.Add(createEnvButton);
-            Controls.Add(textBox1);
-            Controls.Add(envDetailsLabel);
+            Controls.Add(envDetailsValue);
             Controls.Add(envNameValue);
-            Controls.Add(envNameLabel);
             Controls.Add(envCodeValue);
+            Controls.Add(envDetailsLabel);
+            Controls.Add(envNameLabel);
             Controls.Add(envCodeLabel);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(5, 5, 5, 5);
-            Name = "NewEnvForm";
+            Name = "CreateEnvForm";
             Text = "Create Environment";
             ResumeLayout(false);
             PerformLayout();
@@ -123,11 +131,12 @@
         #endregion
 
         private Label envCodeLabel;
-        private TextBox envCodeValue;
-        private TextBox envNameValue;
         private Label envNameLabel;
         private Label envDetailsLabel;
-        private TextBox textBox1;
+        private TextBox envCodeValue;
+        private TextBox envNameValue;
+        private TextBox envDetailsValue;
         private Button createEnvButton;
+        private Label label1;
     }
 }
